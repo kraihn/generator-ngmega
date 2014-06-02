752ac5c6c799d0fb06c583b5c7618309994cda30
Jared Dickson <kraihn@gmail.com>
2014-06-02 17:50:08 -0400

    Add config and routes for app base

--- app/index.js
+++ app/index.js
@@ -7,28 +7,56 @@ var chalk = require('chalk');
 
 
 var NgMegaGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+    init: function () {
+        this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
+        this.on('end', function () {
+            if (!this.options['skip-install']) {
+                this.installDependencies();
+            }
+        });
 
-  app: function () {
-    //this.mkdir('app');
-    //this.mkdir('app/templates');
+        var bower;
+        try {
+            bower = require(path.join(process.cwd(), 'bower.json'));
+        } catch (e) {
+        }
 
-    //this.copy('_package.json', 'package.json');
-    //this.copy('_bower.json', 'bower.json');
-  },
+        this.appname = bower.name || path.basename(process.cwd());
+        this.appname = this._.slugify(this._.humanize(this.appname));
 
-  projectfiles: function () {
-    //this.copy('editorconfig', '.editorconfig');
-    //this.copy('jshintrc', '.jshintrc');
-  }
+        this.scriptAppName = bower.appName || this._.camelize(this.appname) + 'App';
+
+        if (typeof this.env.options.appPath === 'undefined') {
+
+            this.env.options.appPath = bower.appPath || 'app';
+            this.options.appPath = this.env.options.appPath;
+        }
+
+        if (typeof this.env.options.scriptPath === 'undefined') {
+            this.env.options.scriptPath = bower.scriptPath || 'scripts';
+            this.options.scriptPath = this.env.options.scriptPath;
+        }
+    },
+
+    app: function () {
+        //this.mkdir('app');
+        //this.mkdir('app/templates');
+
+        //this.copy('_package.json', 'package.json');
+        //this.copy('_bower.json', 'bower.json');
+        var sourceRoot = '/templates';
+        this.scriptSuffix = '.js';
+
+        this.sourceRoot(path.join(__dirname, sourceRoot));
+        this.template('../../templates/app/app.config.js', path.join(this.env.options.appPath, this.env.options.scriptPath, 'app.config.js'));
+        this.template('../../templates/app/app.routes.js', path.join(this.env.options.appPath, this.env.options.scriptPath, 'app.routes.js'));
+    },
+
+    projectfiles: function () {
+        //this.copy('editorconfig', '.editorconfig');
+        //this.copy('jshintrc', '.jshintrc');
+    }
 });
 
 module.exports = NgMegaGenerator;

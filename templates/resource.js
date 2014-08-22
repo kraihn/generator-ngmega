1bb6a18277759a4061659ea5c68bd88e438bafea
Jared Dickson <kraihn@gmail.com>
2014-08-22 09:36:53 -0400

    Add resource get by id

--- templates/resource.js
+++ templates/resource.js
@@ -16,13 +16,23 @@
 
     var url = config.url;
 
-  /**
-   * Get all <%= cameledName %>es
-   * @returns {Promise}
-   */
-    this.all = function () {
-      return $resource(url, {}, { 'get': { isArray: true }}).get();
-    };
+    /**
+     * Get all <%= cameledName %>es
+     * @returns {Promise}
+     */
+      this.all = function () {
+        return $resource(url, {}, { 'get': { isArray: true }}).get();
+      };
+
+    /**
+     * Get <%= cameledName %> by Id
+     * @returns {Promise}
+     */
+      this.get = function (id) {
+        return $resource(url).get({ id: id });
+      };
+
+      }
 
     }
 

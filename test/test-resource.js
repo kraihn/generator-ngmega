edf1d08f59b01db3606a90b9d26105b429acfc8a
Jared Dickson <code@jareddickson.com>
2014-11-14 18:25:14 -0800

    Add resource tests

---
+++ test/test-resource.js
@@ -0,0 +1,133 @@
+/*global describe, beforeEach, it */
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+
+describe('ngmega:resource', function () {
+
+  describe('common tests', function () {
+
+    describe('simple naming', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:resource', [
+            '../../resource'
+          ], ['hello'], {'common': true});
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected files', function (done) {
+        var expected = [
+          'app/common/services/hello-resource.js'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+
+    describe('complex naming', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:resource', [
+            '../../resource'
+          ], ['hello-world'], {'common': true});
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected files', function (done) {
+        var expected = [
+          'app/common/services/hello-world-resource.js'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('module tests', function () {
+
+    describe('simple naming', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:resource', [
+            '../../resource'
+          ], ['hello', 'there']);
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected files', function (done) {
+        var expected = [
+          'app/scripts/there/services/hello-resource.js'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+
+    describe('complex naming', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:resource', [
+            '../../resource'
+          ], ['hello-world', 'here-there']);
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected files', function (done) {
+        var expected = [
+          'app/scripts/here-there/services/hello-world-resource.js'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+  });
+});

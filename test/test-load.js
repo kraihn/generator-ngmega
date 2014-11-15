c48e8de6cbed895aa5a90f693a688aaeca82f689
Jared Dickson <code@jareddickson.com>
2014-11-14 18:36:30 -0800

    Add script import tests

--- test/test-load.js
+++ test/test-load.js
@@ -7,4 +7,34 @@ describe('ngmega generator', function () {
     var app = require('../app');
     assert(app !== undefined);
   });
+
+  it('module can be imported without blowing up', function () {
+    var module = require('../module');
+    assert(module !== undefined);
+  });
+
+  it('controller can be imported without blowing up', function () {
+    var controller = require('../controller');
+    assert(controller !== undefined);
+  });
+
+  it('directive can be imported without blowing up', function () {
+    var directive = require('../directive');
+    assert(directive !== undefined);
+  });
+
+  it('filter can be imported without blowing up', function () {
+    var filter = require('../filter');
+    assert(filter !== undefined);
+  });
+
+  it('resource can be imported without blowing up', function () {
+    var resource = require('../resource');
+    assert(resource !== undefined);
+  });
+
+  it('service can be imported without blowing up', function () {
+    var service = require('../service');
+    assert(service !== undefined);
+  });
 });

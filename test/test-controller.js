4832ab035959982402683eb220b6c627aa66f9f3
Jared Dickson <code@jareddickson.com>
2014-11-14 18:06:23 -0800

    Add basic controller generation test

---
+++ test/test-controller.js
@@ -0,0 +1,137 @@
+/*global describe, beforeEach, it */
+'use strict';
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+
+describe('ngmega:controller', function () {
+
+  describe('common tests', function () {
+
+    describe('simple naming', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:controller', [
+            '../../controller'
+          ], ['hello'], {'common': true});
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected module files', function (done) {
+        var expected = [
+          'app/common/controllers/hello.js',
+          'app/common/views/hello.html'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+
+    describe('complex naming', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:controller', [
+            '../../controller'
+          ], ['hello-world'], {'common': true});
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected module files', function (done) {
+        var expected = [
+          'app/common/controllers/hello-world.js',
+          'app/common/views/hello-world.html'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('module tests', function () {
+
+    describe('simple naming', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:controller', [
+            '../../controller'
+          ], ['hello', 'there']);
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected module files', function (done) {
+        var expected = [
+          'app/scripts/there/controllers/hello.js',
+          'app/scripts/there/views/hello.html'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+
+    describe('complex naming', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:controller', [
+            '../../controller'
+          ], ['hello-world', 'here-there']);
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected module files', function (done) {
+        var expected = [
+          'app/scripts/here-there/controllers/hello-world.js',
+          'app/scripts/here-there/views/hello-world.html'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+  });
+});

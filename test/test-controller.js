a932481537c7413a2c726993119b7fe9dff42f36
Jared Dickson <code@jareddickson.com>
2015-01-05 09:34:13 -0500

    Add module and controller tests

--- test/test-controller.js
+++ test/test-controller.js
@@ -68,6 +68,68 @@ describe('ngmega:controller', function () {
         });
       });
     });
+
+    describe('complex naming camel', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:controller', [
+            '../../controller'
+          ], ['helloWorld'], {'common': true});
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected files', function (done) {
+        var expected = [
+          'app/common/controllers/hello-world.js',
+          'app/common/views/hello-world.html'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+
+    describe('complex naming class', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:controller', [
+            '../../controller'
+          ], ['HelloWorld'], {'common': true});
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected files', function (done) {
+        var expected = [
+          'app/common/controllers/hello-world.js',
+          'app/common/views/hello-world.html'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
   });
 
   describe('module tests', function () {
@@ -113,6 +175,68 @@ describe('ngmega:controller', function () {
           this.app = helpers.createGenerator('ngmega:controller', [
             '../../controller'
           ], ['hello-world', 'here-there']);
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected files', function (done) {
+        var expected = [
+          'app/scripts/here-there/controllers/hello-world.js',
+          'app/scripts/here-there/views/hello-world.html'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+
+    describe('complex naming camel', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:controller', [
+            '../../controller'
+          ], ['helloWorld', 'hereThere']);
+          done();
+        }.bind(this));
+      });
+
+      it('creates expected files', function (done) {
+        var expected = [
+          'app/scripts/here-there/controllers/hello-world.js',
+          'app/scripts/here-there/views/hello-world.html'
+        ];
+
+        helpers.mockPrompt(this.app, {
+          'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+          helpers.assertFile(expected);
+          done();
+        });
+      });
+    });
+
+    describe('complex naming class', function () {
+      beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+          if (err) {
+            return done(err);
+          }
+
+          this.app = helpers.createGenerator('ngmega:controller', [
+            '../../controller'
+          ], ['HelloWorld', 'HereThere']);
           done();
         }.bind(this));
       });
--- test/test-module.js
+++ test/test-module.js
@@ -66,4 +66,66 @@ describe('ngmega:module', function () {
       });
     });
   });
+
+  describe('complex naming camel', function () {
+    beforeEach(function (done) {
+      helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+        if (err) {
+          return done(err);
+        }
+
+        this.app = helpers.createGenerator('ngmega:module', [
+          '../../module'
+        ], ['helloWorld'], {'common': true});
+        done();
+      }.bind(this));
+    });
+
+    it('creates expected files', function (done) {
+      var expected = [
+        'app/scripts/hello-world/_module.js',
+        'app/scripts/hello-world/hello-world.config.js',
+        'app/scripts/hello-world/hello-world.routes.js',
+        'app/scripts/hello-world/controllers/hello-world.js',
+        'app/scripts/hello-world/views/hello-world.html'
+      ];
+
+      this.app.options['skip-install'] = true;
+      this.app.run({}, function () {
+        helpers.assertFile(expected);
+        done();
+      });
+    });
+  });
+
+  describe('complex naming class', function () {
+    beforeEach(function (done) {
+      helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+        if (err) {
+          return done(err);
+        }
+
+        this.app = helpers.createGenerator('ngmega:module', [
+          '../../module'
+        ], ['HelloWorld'], {'common': true});
+        done();
+      }.bind(this));
+    });
+
+    it('creates expected files', function (done) {
+      var expected = [
+        'app/scripts/hello-world/_module.js',
+        'app/scripts/hello-world/hello-world.config.js',
+        'app/scripts/hello-world/hello-world.routes.js',
+        'app/scripts/hello-world/controllers/hello-world.js',
+        'app/scripts/hello-world/views/hello-world.html'
+      ];
+
+      this.app.options['skip-install'] = true;
+      this.app.run({}, function () {
+        helpers.assertFile(expected);
+        done();
+      });
+    });
+  });
 });
